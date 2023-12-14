1bda5d0ebcd8d30ebb09e31ec293894f87a13f05
Itzii98 <alisson.curay@epn.edu.ec>
2023-12-13 21:15:02 -0500

    TrabajoClase01

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/ACicloVida.kt
@@ -2,10 +2,69 @@ package com.example.b2023gr2sw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
 
 class ACicloVida : AppCompatActivity() {
+    var textoGlobal = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
+        mostrarSnackbar("Hola")
+        mostrarSnackbar("OnCreate")
+
     }
+    override fun onResume() { super.onResume()
+        mostrarSnackbar( "onResume")
+
+    }
+
+    override fun onRestart() { super.onRestart()
+        mostrarSnackbar( "onRestart") }
+
+    override fun onPause() { super.onPause()
+        mostrarSnackbar ("onPause")
+    }
+
+    override fun onStop() { super.onStop()
+        mostrarSnackbar( "onStop")
+    }
+
+    override fun onDestroy() { super.onDestroy()
+        mostrarSnackbar( "onDestroy")
+    }
+
+    fun mostrarSnackbar(texto: String) {
+        textoGlobal += texto
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            textoGlobal, Snackbar.LENGTH_LONG
+        )
+        snack.show()
+        }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outState.run {
+            // guardar las variables
+            //primitivos
+            putString("textoGuardado", textoGlobal)
+            //putInt("numeroGuardado", numero)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        //Recuperar las variables
+        //Primitivos
+        val textoRecuperado:String? = savedInstanceState
+            .getString("TextoGuardado")
+        //val textoRecuperado:Int? = savedInstanceState
+        //.get("numeroguardado")
+        if (textoRecuperado != null){
+            mostrarSnackbar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
+    }
+
 }
--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.b2023gr2sw
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener {
+            //TO DO
+            irActividad(ACicloVida::class.java)
+        }
 
+    }
+    fun irActividad(
+        clase: Class<*>
+    ) {
     }
 }
 
